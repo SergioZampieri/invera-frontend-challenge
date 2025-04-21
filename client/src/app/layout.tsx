b7c3a4ac773dde1fc39561a82e67c0fe2011e903
Sergio <gersio.zampieri@gmail.com>
2025-04-21 04:25:10 -0300

    core project setup

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Sora } from 'next/font/google';
+import './globals.css';
+import React from 'react';
+
+const sora = Sora({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'optional'
+});
+
+export const metadata: Metadata = {
+  title: 'Invera Frontend Challente',
+  description: 'Propuesta de solución al challenge de Invera'
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang='en'>
+      <body className={`${sora.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
